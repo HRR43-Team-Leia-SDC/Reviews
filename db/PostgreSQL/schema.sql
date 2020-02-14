ebff03ba48d742a51b7bbc78da67e0e7b47184c2
ga-possum <aleks.khristof@gmail.com>
2020-02-14 09:32:39 -0500

    psql

---
+++ db/PostgreSQL/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
+
+\c reviews;
+
+CREATE TABLE reviews (
+  id serial NOT NULL,
+  dbId int  ,
+  urlId int  ,
+  username varchar(50)  ,
+  datePosted Date  ,
+  imageUrl varchar(200)  ,
+  avatarImgUrl varchar(200)  ,
+  text varchar(1000)  ,
+  rating real  ,
+  itemForSale varchar(200)  ,
+  PRIMARY KEY (ID)
+);
